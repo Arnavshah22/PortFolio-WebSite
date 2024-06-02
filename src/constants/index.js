454444ad13595c26352ab6d1c29975f65ac55f13
Arnav shah <arnavshaw2015@gmail.com>
2024-06-02 15:01:32 +0530

    Arnav shah

---
+++ src/constants/index.js
@@ -0,0 +1,144 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    contact,
+    css,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    react,
+    redux,
+    sass,
+    tailwindcss,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+    ,{
+        imageUrl:"https://imgs.search.brave.com/szib6dt0v9g-gPbmxN4RFksvfFQuEEdCmwfi_KcMcZE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi8yLzI5L1Bv/c3RncmVzcWxfZWxl/cGhhbnQuc3ZnLzIy/MHB4LVBvc3RncmVz/cWxfZWxlcGhhbnQu/c3ZnLnBuZw",
+        name:"Postgres",
+        type:"Backend"
+    }
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Arnavshah22',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/arnavshah-222004s/',
+    }
+];
+
+export const projects = [
+    {
+       
+        theme: 'btn-back-red',
+        name: 'Venmo Financial App',
+        description: 'Developed a web application That User can Transfer Money From One person To Another',
+        link: 'https://github.com/Arnavshah22/Venmo-Final-Project',
+    },
+    {
+        
+        theme: 'btn-back-green',
+        name: 'Medium-Blogging Site',
+        description: 'Created a Replica of Medium blogging Site that allow User to create and Post Blog.',
+        link: 'https://github.com/Arnavshah22/Medium-blog',
+    },
+]
